7b60c4159f3f109178af9c89ea0e326e74954cf1
Ivan <vanchas@protonmail.com>
2020-03-20 00:36:22 +0200

    Updates

---
+++ precache-manifest.47a779da40706308fe44a3cfadf25f61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2251073650f1d6f9d81de76449bcb5b5",
+    "url": "/DCA/index.html"
+  },
+  {
+    "revision": "d57c3e556e9c2bb6334f",
+    "url": "/DCA/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4bba4596bc5afb06a28c",
+    "url": "/DCA/static/css/main.4a0979e5.chunk.css"
+  },
+  {
+    "revision": "d57c3e556e9c2bb6334f",
+    "url": "/DCA/static/js/2.bd8cf591.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/DCA/static/js/2.bd8cf591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bba4596bc5afb06a28c",
+    "url": "/DCA/static/js/main.4b9abc47.chunk.js"
+  },
+  {
+    "revision": "5dcf2c32940df0faaf56",
+    "url": "/DCA/static/js/runtime-main.fa7550ab.js"
+  }
+]);
